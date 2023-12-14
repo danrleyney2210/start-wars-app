9bcdf15df10c46191ac4a466339c5af229e70287
Danrley Alves <ney@Danrleys-MacBook-Pro.local>
2023-12-14 16:29:51 -0300

    bug: Vercel

---
+++ src/@types/environment.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      REACT_APP_BASE_URL: string;
+    }
+  }
+}
+
+// eslint-disable-next-line prettier/prettier
+export { };
--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://swapi.dev/api",
+  baseURL: "https://swapi.dev/api",
   headers: {
     "Content-Type": "application/json",
   },
